4b35584f25e9e098f02869a6135ea5cf4a523348
Judicael <judi.randriamanjato@gmail.com>
2023-12-05 21:06:45 +0300

    feat: implement new-prime endpoint

---
+++ src/main/java/com/judipoja/prime/service/event/PrimeNumberService.java
@@ -0,0 +1,20 @@
+package com.judipoja.prime.service.event;
+
+import com.judipoja.prime.PojaGenerated;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+@PojaGenerated
+@Service
+@Slf4j
+public class PrimeNumberService {
+  private final SecureRandom secureRandom = new SecureRandom();
+  public String generatePrimeNumber() {
+    BigInteger primeNumber = new BigInteger(1000, 100, secureRandom);
+
+    return primeNumber.toString();
+  }
+}
